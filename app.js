2b57796751acd37531eecc08512272e42c58eafb
เชาวน์  อิ่มคง <52155048+chaoimk@users.noreply.github.com>
2019-08-27 09:18:36 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+/*var mqtt = require('mqtt');  //for mqtt
+var options = {
+    port: 18407,
+    clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
+    username: 'dvbrnzqn',
+    password: 'Znlid1bMKEXd',
+    encoding: 'utf8'
+};
+var client = mqtt.connect('mqtt://m13.cloudmqtt.com',options);
+client.on('connect', function() { // When connected
+  console.log("mqtt connected");
+  
+  
+});
+*/
 app.use(express.static('public'))
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/index.html');
@@ -23,5 +38,7 @@ io.sockets.on('connection', function (socket) {
 function sendanalog(data)
 {
       io.emit('analog', data);
+	  /*var Pt=JSON.parse(data);   //for mqtt
+	  client.publish(Pt.PtName,Pt.PtVal);*/
 	  console.log('data='+data);
 }
